5d567a06c9793628d943ffbd40870a3067235246
Renan Gurgel <gurgel.renan@gmail.com>
2013-10-02 23:38:36 -0300

    Add rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinhaProva::Application.config.secret_key_base = 'a2f04a322a8045e5a7af43a0f321e54f124232f47d0d0b2c4ebb55f315524e4f01c19c3b55eec59f74310a157cb685014055ffc26cc838f397a9976fe9ad2c71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MinhaProva::Application.config.session_store :cookie_store, key: '_minha-prova_session'
