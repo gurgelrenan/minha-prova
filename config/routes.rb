fd979f01f94bc4c52b4e733e409e2036f2c68707
Renan Gurgel <gurgel.renan@gmail.com>
2013-10-07 23:27:10 -0300

    CRUD teachers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MinhaProva::Application.routes.draw do
   
+  resources :teachers
+
   root "home#index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131003024849) do
+ActiveRecord::Schema.define(version: 20131008022037) do
+
+  create_table "teachers", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "teachers", ["user_id"], name: "index_teachers_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
