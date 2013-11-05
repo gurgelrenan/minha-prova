7e6de01ab295381fabe42328cb050880f07da2b4
Renan Gurgel <gurgel.renan@gmail.com>
2013-11-05 09:27:45 -0300

    change deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,42 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# RVM bootstrap
+require 'rvm/capistrano'
+#set :rvm_ruby_string, '1.9.2-p290'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# bundler bootstrap
+require 'bundler/capistrano'
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, "minhaprova.com.br"
+role :web, "domainname"
+role :app, "domainname"
+role :db,  "domainname", :primary => true
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# server details
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :deploy_to, "/var/www/minhaprova.com.br"
+set :deploy_via, :remote_cache
+set :user, "passenger"
+set :use_sudo, false
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# repo details
+set :scm, :git
+set :scm_username, "gurgelrenan"
+set :repository, "git@github.com:gurgelrenan/minha-prova.git"
+set :branch, "master"
+set :git_enable_submodules, 1
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# tasks
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
