81f2c0fd6e5559b77b4a5454b57d97967a5c44c4
Renan Gurgel <gurgel.renan@gmail.com>
2013-11-06 00:43:34 -0300

    improved deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,13 @@
-# RVM bootstrap
-require 'rvm/capistrano'
-#set :rvm_ruby_string, '1.9.2-p290'
+default_run_options[:pty] = true
 
-# bundler bootstrap
-require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
 
 set :application, "minhaprova.com.br"
-role :web, "domainname"
-role :app, "domainname"
-role :db,  "domainname", :primary => true
 
-# server details
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-set :deploy_to, "/var/www/minhaprova.com.br"
-set :deploy_via, :remote_cache
-set :user, "passenger"
-set :use_sudo, false
-
-# repo details
 set :scm, :git
-set :scm_username, "gurgelrenan"
 set :repository, "git@github.com:gurgelrenan/minha-prova.git"
-set :branch, "master"
-set :git_enable_submodules, 1
 
-# tasks
-namespace :deploy do
-  task :start, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
+set :user, "renan"
 
-  task :stop, :roles => :app do
-    # Do nothing.
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-end
+set :stages, ["staging", "production"]
+set :default_stage, "staging"
---
+++ config/deploy/staging.rb
@@ -0,0 +1,2 @@
+server "162.243.39.67", :app, :web, :db, :primary => true
+set :deploy_to, "/var/www/minhaprova.com.br"
