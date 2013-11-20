36cee8e0f7483e745a1fc1ab8cbe4a5b08b7a3b7
Renan Gurgel <gurgel.renan@gmail.com>
2013-11-19 23:32:03 -0300

    change deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,24 @@
-default_run_options[:pty] = true
-
-require 'capistrano/ext/multistage'
-
-set :application, "minhaprova.com.br"
-
-set :scm, :git
-set :repository, "git@github.com:gurgelrenan/minha-prova.git"
+require 'bundler/capistrano'
 
 set :user, "renan"
+set :application, "Minha Prova"
+set :repository,  "git@github.com:gurgelrenan/minha-prova.git"
+set :domain, "minhaprova.com.br"
 
-set :stages, ["staging", "production"]
-set :default_stage, "staging"
+set :scm, :git
+
+role :web, domain 
+role :app, domain 
+role :db, domain,:primary => true
+
+set :deploy_to, "/var/www/minhaprova.com.br"
+set :rails_env, 'production'
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
--- config/deploy/staging.rb
+++
@@ -1,2 +0,0 @@
-server "162.243.39.67", :app, :web, :db, :primary => true
-set :deploy_to, "/var/www/minhaprova.com.br"
