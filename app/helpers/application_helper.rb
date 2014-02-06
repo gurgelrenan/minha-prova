b5a05b0d81ffd03e4cacfde8d435ccc2402caca2
Renan Gurgel <gurgel.renan@gmail.com>
2014-02-06 00:33:56 -0300

    improved login view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,22 @@
 module ApplicationHelper
 	def formata_data(data)
-      # formatando a data: Aug, 31 2007 - 9:55PM
-      data.strftime("%m, %b %Y - %H:%M")
-   end
+    # formatando a data: Aug, 31 2007 - 9:55PM
+    data.strftime("%m, %b %Y - %H:%M")
+  end
+
+  def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success" # Green
+      when :error
+        "alert-danger" # Red
+      when :alert
+        "alert-warning" # Yellow
+      when :notice
+        "alert-info" # Blue
+      else
+        flash_type.to_s
+    end
+  end
+  
 end
